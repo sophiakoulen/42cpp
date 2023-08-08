39efd9e95d9c428ab59d42e9a64af1c990a7591e
Sophia Koulen <sophia.koulen@outlook.com>
2023-08-08 17:02:45 +0200

    Added files for CPP08 - ex00

---
+++ cpp08/ex00/easyfind.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skoulen <skoulen@student.42lausann>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/08 14:29:25 by skoulen           #+#    #+#             */
+/*   Updated: 2023/08/08 14:51:16 by skoulen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_H
+# define EASYFIND_H
+
+template<class Container>
+int	easyfind(Container const& c, int);
+
+# include "easyfind.tpp"
+
+#endif
