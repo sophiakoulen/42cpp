3204e429aa1b616741d7e598c0d8f92689e58d84
Sophia Koulen <sophia.koulen@outlook.com>
2023-08-08 11:40:44 +0200

    CPP06 - made main.cpp a bit more verbose

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -6,20 +6,31 @@
 /*   By: skoulen <skoulen@student.42lausann>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 11:51:20 by skoulen           #+#    #+#             */
-/*   Updated: 2023/08/07 12:39:44 by skoulen          ###   ########.fr       */
+/*   Updated: 2023/08/08 11:40:03 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Data.hpp"
 #include "Serializer.hpp"
 #include <cassert>
+#include <iostream>
 
 int main()
 {
 	char truc[] = "hello world";
 	Data obj(truc);
+
+	std::cout << "Pointer of object: " << &obj << std::endl;
+
 	uintptr_t raw = Serializer::serialize(&obj);
+
+	std::cout << "Serialized version: " << raw << std::endl; 
+	std::cout << "Serialized version in hex: " << std::hex << raw << std::endl;
+
 	Data *ptr = Serializer::deserialize(raw);
+
+	std::cout << "Deserialized version: " << ptr << std::endl;
+
 	assert(&obj == ptr);
 	return (0);
 }
